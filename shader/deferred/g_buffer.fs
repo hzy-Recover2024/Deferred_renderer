2b730cc9b30ec5e0775bce9864dd9330b6ed4781
hzy <hanzy12138@gmail.com>
2026-03-21 23:28:44 +0800

    fix shader bug

--- shader/deferred/g_buffer.fs
+++ shader/deferred/g_buffer.fs
@@ -3,8 +3,8 @@ layout (location = 0) out vec3 gPosition;
 layout (location = 1) out vec3 gNormal;
 layout (location = 2) out vec4 gAlbedoSpec;
 
-in vec2 TexCoords;
 in vec3 FragPos;
+in vec2 TexCoords;
 in vec3 Normal;
 
 uniform sampler2D texture_diffuse1;
